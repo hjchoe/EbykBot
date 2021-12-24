3e23173381a9aa1da7fc72faf4dbf995c0865b6e
James Choe <60300779+hjchoe@users.noreply.github.com>
2021-12-24 00:41:56 -0500

    small slash command self.bot test

--- slashcogs/slashgeneral.py
+++ slashcogs/slashgeneral.py
@@ -1,7 +1,16 @@
 import discord
 import lib.slash_util as slash_util
+import lib.embed
 
 class MyCog(slash_util.ApplicationCog):
+
+    @slash_util.slash_command(description="Test command with latency.")
+    async def test(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        embed = lib.embed.systemEmbed(f"""responding!\n\n**Ping: **{latency}ms""", self.bot)
+        await ctx.send(content=None, embed=embed)
+
+    ##---------- AVATAR -----------##
     @slash_util.slash_command(description="Shows the avatar of the user.")
     @slash_util.describe(user="The user you want avatar of.")
     async def avatar(self, ctx, user: discord.Member): 
