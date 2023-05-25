6618d91b4a7b2dd29e816c03a2a6aab6c57c2899
James Choe <60300779+hjchoe@users.noreply.github.com>
2023-05-25 13:03:16 -0400

    Update EbykBotV2.py

--- EbykBotV2.py
+++ EbykBotV2.py
@@ -89,6 +89,7 @@ def read_token():
 @bot.event
 async def on_message(self, message) -> None:
     print("detected message")
+"""    
     await self.bot.process_commands(message)
 
     if message.author.bot:
@@ -184,6 +185,7 @@ async def on_message(self, message) -> None:
         newNum = oldNum + 1
         c.execute('UPDATE bank SET money = ? WHERE userid = ?', (newNum, message.author.id))
         conn.commit()
+"""
 
 @bot.event
 async def on_voice_state_update(self, member, before, after) -> None:
